ebf887effcb68d08b8fe5b897aaccee8e2517ea9
Nachimuthu Vellingiri <nacvelli@publicisgroupe.net>
2018-11-28 16:11:32 +0000

    Counter App using Vuex

---
+++ src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class = "counter container">
+    <div class = "row text-center">
+      <h1>{{$store.state.counter.count}}</h1>, Counter is {{ IsEvenOrOdd }}</h3>
+      <h5>Total Clicks: {{$store.state.counter.click}}</h5>
+
+      <button class = "btn btn-success" @click = "increment" :disabled = IsClickLimitDone>+</button>
+      <button class = "btn btn-danger" @click = "decrement" :disabled = IsClickLimitDone>-</button>
+      <button class = "btn btn-info" @click = "incrementIfOdd" :disabled = IsClickLimitDone>Increment if odd</button>
+      <button class = "btn btn-info" @click = "incrementAsync" :disabled = IsClickLimitDone>Increment aync</button>
+
+      <p class = "text-danger" v-show = "IsClickLimitDone">You have reached maximum click limit! Please reset counter.</p>
+      <button class = "btn btn-warning" v-show="IsClickLimitDone" @click = "enableButtons">Reset Counter</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Counter',
+  computed: mapGetters([
+    'IsEvenOrOdd',
+    'IsClickLimitDone'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync',
+    'enableButtons'
+  ])
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  padding: 25px 0 0;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const counter = {
+  state : {
+    count: 0,
+    click: 0,
+    maxClick: 10
+  },
+
+  getters : {
+    IsEvenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd',
+    IsClickLimitDone: state => state.click >= state.maxClick ? true : false
+  },
+
+  mutations : {
+    increment (state) {
+      state.count++;
+      state.click++;
+    },
+    decrement (state) {
+      state.count--;
+      state.click++;
+    },
+    reset (state) {
+      state.count = 0;
+      state.click = 0;
+    }
+  },
+
+  actions : {
+    increment: ({commit}) => commit('increment'),
+    decrement: ({commit}) => commit('decrement'),
+    incrementIfOdd: ({commit, state}) => {
+      if((state.count + 1) % 2 === 0) {
+        commit('increment');
+      }
+    },
+    incrementAsync: ({ commit }) => {
+      return new Promise((response, reject) => {
+        setTimeout(() => {
+          commit('increment')
+          resolve()
+        }, 1000)
+      })
+    },
+    enableButtons: ({commit, state}) => {
+      commit('reset')
+    }
+  }
+}
+
+const weather = {
+  state: {
+    temp: 23
+  }
+}
+
+export default new Vuex.Store({
+  modules: {
+    counter: counter,
+    weather: Weather
+  }
+})
